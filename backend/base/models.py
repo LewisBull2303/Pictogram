6d385984f9f9df5c96ed13f2f2cd55071ff85c75
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-03 14:12:59 +0000

    Added 3 attributes to my users, one is their profile image which is an uploaded image which will be stored in the  back end, the next is a bio, where the user can add up to 500 characters to describe themselves like on other social medias, and a follower amount which is a manytomany field so that you can follow others and they can follow you, it is not symmetrical as a user can follow someone but they may not follow them back. And a related name of following for when you are already following someone

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class MyUser(AbstractUser):
-    
+    bio = models.Charfield(max_length=500)
+    profile_image = models.ImageField(upload_to='profile_image/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
