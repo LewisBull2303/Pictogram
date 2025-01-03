05f4465eefedb3fc4507052a7648495b4d0c31f8
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-03 16:19:54 +0000

    Added the static image url to the urls.py under the base

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
