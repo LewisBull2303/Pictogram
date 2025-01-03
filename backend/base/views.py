5dd2dcecad2e94cf5763f9ed054bbf8d94004470
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-03 16:34:24 +0000

    Added the api_view to the views and made a function to the get the users profile data and see if the username does exist, if it does not the program will throw an error 2024

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,4 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Users
+from .serializers import UserProfileSerializer
+
 # Create your views here.
+@api_view(['GET'])
+def get_user_profile_data(request, pk):
+    try:
+        user = Users.objects.get(username=pk)
+    except Users.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
 
