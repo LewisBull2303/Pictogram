7fd6f179d1cc32ace6034e4efc25c46a88068551
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-03 14:54:29 +0000

    Added the followers count to the serilizer to get the number if followers from model.py and created a function which will return the follower count, i then created the following count variable to do the same with later

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Users
 
 class UserProfileSerializer(serializers.ModelSerializer):
+
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Users
-        fields = ['username', 'bio', 'profile_image']
+        fields = ['username', 'bio', 'profile_image', 'follower_count']
+
+    def get_follower_count(self, obj):
+        return obj.followers.count()
