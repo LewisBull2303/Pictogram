f3e8ada18010369d0e5bb0c35b862f3ed2f2f9bf
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-03 16:48:10 +0000

    fixed a couple of spelling mistakes where capitalization was not use when it should be in models.py

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Users(AbstractUser):
-    username = modelsCharfield(max_length=50, unique=True, primary_key=True)
-    bio = models.Charfield(max_length=500)
+    username = modelsCharField(max_length=50, unique=True, primary_key=True)
+    bio = models.CharField(max_length=500)
     profile_image = models.ImageField(upload_to='profile_image/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
