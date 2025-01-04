5efbd53a7ba4f429cca346471b6f3d5e765ce5f2
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-04 16:26:12 +0000

    Added some allowed hosts for the server to run locally and added a super user, Additionally I made it so that the admin can see the users in the admin dashboard

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-84#_z(!(u5japvrm%mm=#zc%fy!!ot5eqaf@hj!d)w@eu#6g43
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
 AUTH_USER_MODEL = 'base.Users'
 
