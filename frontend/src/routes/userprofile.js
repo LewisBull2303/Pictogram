ca097b1d195e4a3da685836016cbc30a15362a2b
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-04 20:48:20 +0000

    Created the start of my user profile app, imported all the react packages that I need such as react-router-dom and @chakra-ui/react to make my project, I have made a routes folder have put the route to my user profile in there so I can reference it in my app.js

---
+++ frontend/src/routes/userprofile.js
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react'
+
+const UserProfile = () => {
+    return (
+        <Text>
+            User Profile
+        </Text>
+    )
+}
+
+
+export default UserProfile
