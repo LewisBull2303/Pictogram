78df3483db4920190378e1cc8eda70bf1ffe0324
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-04 20:57:07 +0000

    Added layout.js with a simple child in it for now, I have imported it into my app.js also

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import Layout from './routes/components/layout';
+
 import UserProfile  from './routes/userprofile';  
 
 function App() {
