dba04611f4209230b035af9b9ca8234606b690fe
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-04 21:15:00 +0000

    Imported the Navbar into the layout.js to see if it is working correctly, and it is, the NavBar is at the top of the screen in the middle

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -1,8 +1,11 @@
 import { Box, VStack } from '@chakra-ui/react'
 
+import NavBar from './navbar'
+
 const Layout = ({children}) => {
     return (
         <VStack w='100vw' minH='100vh' bg='#FCFCFC'>
+            <NavBar/>
             <Box w='100vw'>
                 {children}
             </Box>
