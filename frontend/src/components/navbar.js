8b02b32b489eeb0e8da5479b1994006602c38f8f
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-04 21:13:15 +0000

    Added the navbar.js componant which I can now begin styling the Navbar on, I added a flex and Text to test later if it is working

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,11 @@
+import { Flex, Text} from "@chakra-ui/react";
+
+const NavBar = () => {
+    return (
+        <Flex>
+            <Text>NavBar</Text>
+        </Flex>
+    )
+}
+
+export default NavBar;
