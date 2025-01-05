55451e3984fe8d07846a783f83f59dd8a29bee42
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-05 11:31:49 +0000

    Added styling to my Navbar, I made is 100% of the view width and made it so tha it was centered, I then added space between to put my logo/name of the website on the left and all of the other options on the right to make the websitre more modern, I added font size to the name of the website and settled on the name clonestagram as this is an instagram clone I am making, I added some more color to make the page look more lively

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,9 +1,14 @@
-import { Flex, Text} from "@chakra-ui/react";
+import { Flex, HStack, Text} from "@chakra-ui/react";
 
 const NavBar = () => {
     return (
-        <Flex>
-            <Text>NavBar</Text>
+        <Flex w='100vw' h='90px' bg='blue.600' justifyContent="center" alignItems='center'>
+            <HStack w='90%' justifyContent='space-between' color='white'>
+                <Text fontSize='24px' fontWeight='bold'>Clonestagram</Text>
+                <HStack>
+                    <Text>Profile</Text>
+                </HStack>
+            </HStack>
         </Flex>
     )
 }
