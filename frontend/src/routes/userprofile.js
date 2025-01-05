de455b60ead600483f7a009cc0d908b93c8fc1b0
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-05 20:14:10 +0000

    Added the username being the same as the url which will take people to their own profile and will automatically populate the username, Additionally added some extra styling such as gaps and a button and bio which will get functionallity soon

--- frontend/src/routes/userprofile.js
+++ frontend/src/routes/userprofile.js
@@ -1,28 +1,41 @@
-import { Text, VStack, Flex, Box, Heading, HStack, Image} from '@chakra-ui/react'
+import { Text, VStack, Flex, Box, Heading, HStack, Image, Button} from '@chakra-ui/react'
+import { useEffect, useState } from 'react';
 
 const UserProfile = () => {
+
+    const get_username_from_url = () => {
+        const split_url = window.location.pathname.split('/');
+        return split_url[split_url.length - 1];
+    }
+
+    const [username, setUsername] = useState(get_username_from_url())
+
+    useEffect(() =>{
+        setUsername(get_username_from_url())
+    }, [])
+
     return (
         <Flex w='100%' justifyContent='center'>
             <VStack w='75%' >
                 <Box w='100%' mt='40px'>
-                    <UserDetails />
+                    <UserDetails username={username}/>
                 </Box>
             </VStack>
         </Flex>
     )
 }
 
-const UserDetails = () => {
+const UserDetails = ({username}) => {
     return (
-        <VStack w='100%' alignItems='start'>
-            <Heading>@admin33</Heading>
-            <HStack>
+        <VStack w='100%' alignItems='start' gap='40px'>
+            <Heading>@{username}</Heading>
+            <HStack gap='20px'>
                 <Box boxSize='150px' border='2px solid' borderColor='grey.700' bg='white' borderRadius='full' overflow='hidden'> 
                 <Image src='https://8000-lewisbull2303-instagram-spjqg3hzjk.app.codeanywhere.com/api//media/profile_image/360_F_7324855_mx4CEBWTr81XLOrlQccCROtP2uNR7xbk.jpg' boxSize='100%' objectFit='cover'/>
 
                 </Box>
-                <VStack>
-                    <HStack>
+                <VStack gap='20px'>
+                    <HStack gap='20px' fontSize='18px'>
                         <VStack>
                             <Text>Followers</Text>
                             <Text>0</Text>
@@ -32,9 +45,10 @@ const UserDetails = () => {
                             <Text>0</Text>
                         </VStack>
                     </HStack>
+                    <Button w='100%'>Edit Profile</Button>
                 </VStack>
             </HStack>
-
+            <Text fontSize='18px'>Bio</Text>
         </VStack>
     )
 }
