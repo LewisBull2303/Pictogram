0b0ad04edfe754d2a76eb1a9778cb5567e03027b
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-05 11:37:12 +0000

    Started a function to handle the naviagtion, currently there is only the profile so I have made it so that when the user clicks on the profile text It will take them to their profile, I just need to import and get the username of the person logged in

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,20 @@
 import { Flex, HStack, Text} from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+
+    const nav = useNavigate();
+
+    const handleNavigation = (route) => {
+        nav(`/${route}`)
+    }
+
     return (
         <Flex w='100vw' h='90px' bg='blue.600' justifyContent="center" alignItems='center'>
             <HStack w='90%' justifyContent='space-between' color='white'>
                 <Text fontSize='24px' fontWeight='bold'>Clonestagram</Text>
                 <HStack>
-                    <Text>Profile</Text>
+                    <Text onClick={(route) => handleNavigation('/')}>Profile</Text>
                 </HStack>
             </HStack>
         </Flex>
