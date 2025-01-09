faa28169099e54e6dc24da6305095f5a33abb71c
LewisBull2303 <lewisbull2303@gmail.com>
2025-01-09 16:22:05 +0000

    Finalised storing the tokens in cookies so now the admin can all the tokens to access api data

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -20,15 +20,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import get_user_profile_data
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import get_user_profile_data, CustomTokenObtainPairView, CustomTokenRefreshView
 
 urlpatterns = [
     path('user_data/<str:pk>/', get_user_profile_data),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/base/views.py
+++ backend/base/views.py
@@ -12,24 +12,67 @@ from rest_framework_simplejwt.views import (
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        tokens = response.data
+        try:
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
 
-        access_token = tokens['access']
-        refresh = tokens['refresh']
+            access_token = tokens['access']
+            refresh_token = tokens['refresh']
 
-        res = Response()
+            res = Response()
 
-        res.data = {"success":True}
+            res.data = {"success":True}
 
-        res.set_cookie(
-            key='access_token',
-            value=access_token,
-            httponly=True,
-            secure=True,
-            samesite='None',
-            path='/'
-        )
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            res.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            return res
+        except:
+            return Response({'success':False})
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            request.get['refresh'] = refresh_token
+
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+
+            access_token = tokens['access']
+
+            res.data = {"success":True}
+
+            res = Response();
+
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            return res
+        except:
+            return Response({'success':False})
+
 
 # Create your views here.
 @api_view(['GET'])
