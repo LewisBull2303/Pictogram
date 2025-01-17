8e781b65c4c73cfda2d8daa9d4c600d7fe348ffc
Lewis Bull <lewisbull2303@gmail.com>
2025-01-17 17:28:57 +0000

    Started work on the register page

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 from .models import Users
 
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only = True)
+
+    class Meta:
+        model = Users
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+
+        def create(self, validated_data):
+            user = Users(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name']
+            )
+
 class UserProfileSerializer(serializers.ModelSerializer):
 
     follower_count = serializers.SerializerMethodField()
