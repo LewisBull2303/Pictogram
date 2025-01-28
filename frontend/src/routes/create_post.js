feb16c38a99169e8ca95442f065b4eadeae0cf77
Lewis Bull <lewisbull2303@gmail.com>
2025-01-28 17:14:56 +0000

    Currently trying to set up the create post page but am getting an issue with the image url not coming up correctly, Will attempt to bug fix the code now

--- frontend/src/routes/create_post.js
+++ frontend/src/routes/create_post.js
@@ -1,15 +1,43 @@
-import { Text, VStack, Flex, Heading, FormControl, FormLabel, Input, Button, Image} from "@chakra-ui/react"
+import { VStack, Flex, Heading, FormControl, FormLabel, Input, Button,} from "@chakra-ui/react"
+import { create_post } from '../api/endpoints'
+import { useState } from "react"
 
 const CreatePost = () => {
+
+    const [image, setImage] = useState('')
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0]
+        if(file) {
+            setImage(file)
+        }
+    }
+
+    const handlePost = async () => {
+        if (!image) {
+            alert('Please select and Image First')
+            return
+        }
+
+        const formData = new FormData()
+        formData.append('post_image', image)
+        try {
+            await create_post(formData)
+        }
+        catch{
+            alert('Error Creating your post')
+        }
+    }
+
     return (
         <Flex w='100%' h='100%' justifyContent='center' pt='50px'>
             <VStack w='95%' maxWidth = '450px' gap='40px'>
                 <Heading>Create a Post!</Heading>
-                <FormControl>
-                    <FormLabel>Upload a Picture</FormLabel>
-                    <Input bg='white' type='file' name='post_image' accept='image/jpeg, image/png'/>
+                <FormControl textAlign='center'>
+                    <FormLabel textAlign='center'>Upload a Picture</FormLabel>
+                    <Input onChange={handleFileChange} bg='white' type='file' name='post_image' accept='image/jpeg, image/png'/>
                 </FormControl>
-                <Button w='100%' colorScheme='blue'>Create Post</Button>
+                <Button onClick={handlePost}w='100%' colorScheme='blue'>Create Post</Button>
             </VStack>
         </Flex>
     )
