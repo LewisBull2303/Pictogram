177f8bcb2932d8e39b0aa7899d3598d614fff6d6
Lewis Bull <lewisbull2303@gmail.com>
2025-01-30 15:54:49 +0000

    Added some styles to my create post page and added a plus button to make a new post whenever you want

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,6 +2,7 @@ import { Flex, HStack, Text} from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom'
 
 import { CgProfile } from "react-icons/cg";
+import { IoAddCircleOutline } from "react-icons/io5";
 
 const NavBar = () => {
 
@@ -15,8 +16,9 @@ const NavBar = () => {
         <Flex w='100vw' h='90px' bg='blue.600' justifyContent="center" alignItems='center'>
             <HStack w='90%' justifyContent='space-between' color='white'>
                 <Text fontSize='24px' fontWeight='bold'>Clonestagram</Text>
-                <HStack>
+                <HStack gap='20px'>
                     <Text onClick={(route) => handleNavigation('')}><CgProfile size='22px'/></Text>
+                    <Text onClick={(route) => handleNavigation('create/post')}><IoAddCircleOutline size='22px' /></Text>
                 </HStack>
             </HStack>
         </Flex>
