434440fb593faf35a103108290c3e7378b008847
Lewis Bull <lewisbull2303@gmail.com>
2025-02-03 12:41:23 +0000

    Created my search user function to now return the username of the people the query relates to however it looks bad and the user cannot click on the users to view their profile at the moment

--- frontend/src/routes/search.js
+++ frontend/src/routes/search.js
@@ -1,16 +1,31 @@
 import { Flex, HStack, VStack, Heading, Input, Button } from "@chakra-ui/react"
+import { useState } from "react"
+import { search_users } from "../api/endpoints"
 
 const Search = () => {
+
+    const [search, setSearch] = useState('')
+    const [users, setUsers] = useState([])
+
+    const handleSearch = async () => {
+        const users = await search_users(search)
+        setUsers(users)
+    }
+
     return (
         <Flex w='100%' justifyContent='center' pt='50px'>
             <VStack w='95%' maxWidth='500px' gap='20px'>
                 <Heading>Search for Other Users!</Heading>
                 <HStack w='100%' gap='0'>
-                    <Input/>
-                    <Button>Search</Button>
+                    <Input onChange={(e) => setSearch(e.target.value)} bg='white'/>
+                    <Button onClick={handleSearch} colorScheme="blue">Search</Button>
                 </HStack>
                 <VStack w='100%'>
-                    
+                    {
+                        users.map((users) => {
+                            return <Heading>{users.username}</Heading>
+                        })
+                    }
                 </VStack>
             </VStack>
         </Flex>
