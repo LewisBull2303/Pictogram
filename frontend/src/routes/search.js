77e68efff780bceb8d7f7d51651dc28b4431520f
Lewis Bull <lewisbull2303@gmail.com>
2025-02-03 12:35:24 +0000

    Added some basic styles and fixed a small spelling mistake, Also fixed an error that made it so the code was unreachable, I move the parenthesis up next to the return

--- frontend/src/routes/search.js
+++ frontend/src/routes/search.js
@@ -1,16 +1,15 @@
 import { Flex, HStack, VStack, Heading, Input, Button } from "@chakra-ui/react"
 
 const Search = () => {
-    return 
-    (
-        <Flex>
-            <VStack>
-                <Heading>Seach for Other Users!</Heading>
-                <HStack>
+    return (
+        <Flex w='100%' justifyContent='center' pt='50px'>
+            <VStack w='95%' maxWidth='500px' gap='20px'>
+                <Heading>Search for Other Users!</Heading>
+                <HStack w='100%' gap='0'>
                     <Input/>
                     <Button>Search</Button>
                 </HStack>
-                <VStack>
+                <VStack w='100%'>
                     
                 </VStack>
             </VStack>
