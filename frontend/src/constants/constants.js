f31f787c4257f7893b8e47701014a6b2c7254bed
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2025-02-13 12:49:44 +0000

    Update constants.js

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'pictogram-project5-4fab6a1a47d8.herokuapp.com/api'
+export const SERVER_URL = 'https://pictogram-project5-4fab6a1a47d8.herokuapp.com/api'
 
 console.log(SERVER_URL);
