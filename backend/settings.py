5f86246115173b276593d3c10595283c3df28239
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2025-02-13 11:33:39 +0000

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pictogram-project5-4fab6a1a47d8.herokuapp.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['https://localhost:3000', 'pictogram-project5-4fab6a1a47d8.herokuapp.com']
 
 AUTH_USER_MODEL = 'base.Users'
 
