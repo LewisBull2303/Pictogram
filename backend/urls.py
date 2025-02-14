4a4e735c9aa277aaa805e33ac91ad1f1f66f7d52
Lewis Bull <lewisbull2303@gmail.com>
2025-02-14 11:51:38 +0000

    Changed the urls.py to fix a bug with the authenticate not working correctly

--- backend/urls.py
+++ backend/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path("admin/", admin.site.urls),
-    path('api/', include('base.urls')),
+    path('/api/', include('base.urls')),
 
 ]
 
