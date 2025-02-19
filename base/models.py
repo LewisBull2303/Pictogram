080d6c454649be9c18d0f3266ee6c47b2d806073
Lewis Bull <lewisbull2303@gmail.com>
2025-02-19 13:24:32 +0000

    Changed the default image url

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class Users(AbstractUser):
     username = models.CharField(max_length=50, unique=True, primary_key=True)
     bio = models.CharField(max_length=500)
-    profile_image = models.ImageField(upload_to='images/', default='Default_pfp.jpg')
+    profile_image = models.ImageField(upload_to='images/', default='../Default_pfp.jpg')
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
     def __str__(self):
